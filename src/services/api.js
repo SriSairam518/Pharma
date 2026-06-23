c76df0cfebefd9b69d55cfb5c695a0df12050038
SriSairam518 <srisairamvalakati@gmail.com>
2026-06-23 10:58:28 +0530

    remove /api from app.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_URL + "/api";
+const BASE_URL = import.meta.env.VITE_URL;
 
 const apiClient = axios.create({
     baseURL: BASE_URL,
